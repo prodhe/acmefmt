8c73f51e4487eb1155080a7e0c2b006b2df330b6
Petter Rodhelind <petter.rodhelind@gmail.com>
2020-01-15 15:57:22 +0100

    Initial commit.

---
+++ main.go
@@ -0,0 +1,184 @@
+// Acmefmt watches acme for different file suffixes and runs
+// the appropriate code formatting tool for each file upon write.
+// If formatting takes place, it is directly written to the body in
+// acme but does not write the file.
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"9fans.net/go/acme"
+)
+
+func main() {
+	flag.Parse()
+	l, err := acme.Log()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		event, err := l.Read()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if event.Name != "" && event.Op == "put" && strings.HasSuffix(event.Name, ".html") {
+			reformat(event.ID, event.Name, "html-beautify", "-t", "-f", event.Name)
+		}
+		if event.Name != "" && event.Op == "put" && strings.HasSuffix(event.Name, ".js") {
+			reformat(event.ID, event.Name, "js-beautify", "-t", "-f", event.Name)
+		}
+		if event.Name != "" && event.Op == "put" && strings.HasSuffix(event.Name, ".css") {
+			reformat(event.ID, event.Name, "css-beautify", "-t", "-f", event.Name)
+		}
+	}
+}
+
+func reformat(id int, name string, cmd ...string) {
+	w, err := acme.Open(id, nil)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer w.CloseFiles()
+
+	old, err := ioutil.ReadFile(name)
+	if err != nil {
+		//log.Print(err)
+		return
+	}
+	new, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+	if err != nil {
+		if strings.Contains(string(new), "fatal error") {
+			fmt.Fprintf(os.Stderr, "%s: %s: %v\n%s", cmd[0], name, err, new)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s", new)
+		}
+		return
+	}
+
+	if bytes.Equal(old, new) {
+		return
+	}
+
+	f, err := ioutil.TempFile("", "acmefmt")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if _, err := f.Write(new); err != nil {
+		log.Print(err)
+		return
+	}
+	tmp := f.Name()
+	f.Close()
+	defer os.Remove(tmp)
+
+	diff, _ := exec.Command("9", "diff", name, tmp).CombinedOutput()
+
+	latest, err := w.ReadAll("body")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if !bytes.Equal(old, latest) {
+		log.Printf("skipped update to %s: window modified since Put\n", name)
+		return
+	}
+
+	w.Write("ctl", []byte("mark"))
+	w.Write("ctl", []byte("nomark"))
+	diffLines := strings.Split(string(diff), "\n")
+	for i := len(diffLines) - 1; i >= 0; i-- {
+		line := diffLines[i]
+		if line == "" {
+			continue
+		}
+		if line[0] == '<' || line[0] == '-' || line[0] == '>' {
+			continue
+		}
+		j := 0
+		for j < len(line) && line[j] != 'a' && line[j] != 'c' && line[j] != 'd' {
+			j++
+		}
+		if j >= len(line) {
+			log.Printf("cannot parse diff line: %q", line)
+			break
+		}
+		oldStart, oldEnd := parseSpan(line[:j])
+		newStart, newEnd := parseSpan(line[j+1:])
+		if oldStart == 0 || newStart == 0 {
+			continue
+		}
+		switch line[j] {
+		case 'a':
+			err := w.Addr("%d+#0", oldStart)
+			if err != nil {
+				log.Print(err)
+				break
+			}
+			w.Write("data", findLines(new, newStart, newEnd))
+		case 'c':
+			err := w.Addr("%d,%d", oldStart, oldEnd)
+			if err != nil {
+				log.Print(err)
+				break
+			}
+			w.Write("data", findLines(new, newStart, newEnd))
+		case 'd':
+			err := w.Addr("%d,%d", oldStart, oldEnd)
+			if err != nil {
+				log.Print(err)
+				break
+			}
+			w.Write("data", nil)
+		}
+	}
+}
+
+func parseSpan(text string) (start, end int) {
+	i := strings.Index(text, ",")
+	if i < 0 {
+		n, err := strconv.Atoi(text)
+		if err != nil {
+			log.Printf("cannot parse span %q", text)
+			return 0, 0
+		}
+		return n, n
+	}
+	start, err1 := strconv.Atoi(text[:i])
+	end, err2 := strconv.Atoi(text[i+1:])
+	if err1 != nil || err2 != nil {
+		log.Printf("cannot parse span %q", text)
+		return 0, 0
+	}
+	return start, end
+}
+
+func findLines(text []byte, start, end int) []byte {
+	i := 0
+
+	start--
+	for ; i < len(text) && start > 0; i++ {
+		if text[i] == '\n' {
+			start--
+			end--
+		}
+	}
+	startByte := i
+	for ; i < len(text) && end > 0; i++ {
+		if text[i] == '\n' {
+			end--
+		}
+	}
+	endByte := i
+	return text[startByte:endByte]
+}
